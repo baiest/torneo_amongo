a04d2a26aeb62408b0994534c8923a3a7a33138e
baiest <ballesteros.juan@correounivalle.edu.co>
2020-12-12 03:05:04 -0500

    tablas y botones agregados

--- torneo-amongo.py
+++ torneo-amongo.py
@@ -6,8 +6,8 @@ from flask import Flask
 from flask import request, jsonify, render_template
 app = Flask(__name__)
 
-
-def tabla():
+@app.route('/', methods=["GET", "POST"])
+def index():
     scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
@@ -25,24 +25,31 @@ def tabla():
 
 
     flag = True
-    i = 12
-    participantes = []
+    i = 11
+    participante = {}
+    datos = worksheet.get_all_values()
+    print(datos[12][0])
     while flag:
-        nombre = worksheet.cell(i, 1).value
-        punto = worksheet.cell(i, 9).value
-        if nombre != "":
-            persona = {nombre : punto}
-            participantes.append(persona)
-            print(persona)
-        else:
+        try:
+            participante.update({datos[i][0] : int(datos[i][8])})
+        except:
             flag = False
         i+=1
-    print(participantes)
-    return jsonify(participantes)
+    print(participante)
 
-@app.route('/')
-def index():
-    participantes = tabla()
-    return render_template('./public/index.html')
+    i=11 # numero en el excel
+    if request.method == "POST":
+        name = request.form['name']
+        print(name)
+        while datos[i][0] != name:
+            i+=1    
+        datos[i][1] = str(int(datos[i][1]) + 1)
+        participante[name] +=1
+        worksheet.update_cell(i+1, 2, str(int(datos[i][1])))
+    
+    listaLetra = datos[10][1:6]
+    print(listaLetra)
+    return render_template('index.html', tabla= participante, lista = list(participante.keys()), listaLetra = listaLetra)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 5000)
